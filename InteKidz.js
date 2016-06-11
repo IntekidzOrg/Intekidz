c5dc1cc2035205a276e59673f15619f49d0e3dcf
Jana Zeller <jana.zeller@googlemail.com>
2016-06-11 15:12:11 +0200

    Revert "Merge branch 'master' into gh-pages"
    This reverts commit 3ef34df3e83161504821a6fd23a78b2c811990cb, reversing
    changes made to f3af208f2cf6abd7b6da09c985e0c78e8a81d4b7.

--- InteKidz.js
+++ InteKidz.js
@@ -28,9 +28,10 @@ function intekidzlogin(){
 							alert(text[lang][0]);
 							break;
 						}
-			} else {
-				alert(text[lang][1]);
 			}
+		}
+	if(userId === "null"){
+			alert(text[lang][1]);
 		}
 	}
 function intekidzlogout(){

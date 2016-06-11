d23f4946615de975fb7f279270fcadc694a817b7
JoanaMana <jnbergsiek@gmail.com>
2016-06-11 12:37:34 +0200

    changed stuff

--- InteKidz.js
+++ InteKidz.js
@@ -1,5 +1,5 @@
 //Variablen
-var datenbank = [["a", "b"], ["c", "d"]]; //namen, passwörter
+var datenbank = [["Peter", "Jon", "Claudia"], ["1234", "JonJon","qwertz"]]; //namen, passwörter
 var text = [["Falsches Passwort oder Benutzername",
              "Benutzername wurde nicht gefunden",
              "Ausgeloggt"], 
@@ -12,39 +12,31 @@ var text = [["Falsches Passwort oder Benutzername",
 var lang = 0; //0 = Deutsch; 1 = Englisch; 2 = Arabisch
 var username = document.getElementById("username"); //eingegebener Name
 var password = document.getElementById("password"); //eingegebenes PW
-var userId = null; //eingeloggter Benutzer
+var userId = "null"; //eingeloggter Benutzer
 //Funktionen
 function changeLang(pLang) {//Sprache wechseln
     "use strict";
     lang = pLang; }
 var i;
-function intekidzlogin()
-{
-	for(i = 0; i < datenbank[0].length; i++ ) //gehe gesp. Namen durch
-	{
-		if(datenbank[0][i] == username) //einge. Name ist in Dateb
-			{
-				if(datenbank[1][i] == password) //to do: enkrypten
-					{
+function intekidzlogin(){
+	for(i = 0; i < datenbank[0].length; i++ ) { //gehe gesp. Namen durch
+		if(datenbank[0][i] === username) { //einge. Name ist in Dateb
+				if(datenbank[1][i] === password) { //to do: enkrypte
 						userId = datenbank[0][i];
 						break;
-					} else
-						{
+					} else {
 							alert(text[lang][0]);
 							break;
 						}
 			}
 		}
-	if(userId === null)
-		{
+	if(userId === "null"){
 			alert(text[lang][1]);
 		}
 	}
-function intekidzlogout()
-{
-	if(userId !== null)
-		{
-			userId = null;
+function intekidzlogout(){
+	if(userId !== "null"){
+			userId = "null";
 			alert(text[lang][2])
 		}
 }
